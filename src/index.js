dda6b82b38a5d4d7bf7e91108610266d44dd8470
realEdgar <ing.edgar.lopez.28@gmail.com>
2021-05-09 13:53:42 -0500

    Changin yarn as a package default

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello world from JS");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello Guy from react</h1>, 
+    document.getElementById('app')
+);
