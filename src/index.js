de1455014ce37175cb6ac12285acb4b48b660277
realEdgar <ing.edgar.lopez.28@gmail.com>
2021-05-13 09:13:21 -0500

    Adding dependencies to handle de backend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
 import { App } from './App'
 
+const client = new ApolloClient({
+  url: 'URL_DEL_BACKEND'
+})
+
 ReactDOM.render(
-  <>
+  <ApolloProvider client={client}>
     <App />
-  </>,
+  </ApolloProvider>,
   document.getElementById('app')
 )
