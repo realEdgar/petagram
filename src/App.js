03ca5136496541ca2f20727c44883cf5d97da50b
realEdgar <ing.edgar.lopez.28@gmail.com>
2021-05-15 09:45:49 -0500

    Adding routes protected

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import { GlobalStyles } from './styles/GlobalStyles'
 import { Logo } from './components/Logo/index'
 
 import { Home } from './pages/Home'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisterUser } from './pages/NotRegisterUser'
 import { NavBar } from './components/NavBar/index'
 
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 export const App = () => {
   return (
     <>
@@ -13,8 +19,23 @@ export const App = () => {
       <Logo />
       <Router>
         <Home path='/' />
-        <Home path='/pet/:id' />
+        <Home path='/pet/:id' />        
       </Router>
+
+      <UserLogged>
+        {
+          ({ isAuth }) => 
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              : <Router>
+                <NotRegisterUser path='/favs' />
+                <NotRegisterUser path='/user' />
+              </Router>
+        } 
+      </UserLogged>
       <NavBar />
     </>
     
